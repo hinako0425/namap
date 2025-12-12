fb352a1d132f20b6d6775e4c875034f6666837b0
3A21 <3a21@3A21.local>
2025-12-12 12:51:35 +0900

    テスト

--- build.sh
+++ build.sh
@@ -10,3 +10,8 @@ python3 manage.py collectstatic --no-input
 
 #3.データベースのマイグレーション(本番DBにテーブル作成)
 python3 manage.py migrate
+
+if[[-n "$DJANGO_SUPERUSER_USERNAME"]];then
+    echo "Creating superuser..."
+    python3 manage.py createsuperuser --noinput||echo "Superuser already exists."
+fi
